70e3f69b56b458212633169845bba433c73a4cbc
Avila Fernando <fernando.avila@deprati.com>
2021-12-30 16:55:47 -0500

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -56,13 +56,16 @@ namespace TailSpin.SpaceGame.Web
             int page = 1, int pageSize = 10
         )
         {
-            var result = _items
-                .Where(queryPredicate) // filter
-                .OrderByDescending(orderDescendingPredicate) // sort
-                .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+ 
 
-            return Task<IEnumerable<T>>.FromResult(result);
+            var result = _items.AsQueryable()
+        .Where(queryPredicate) // filter
+        .OrderByDescending(orderDescendingPredicate) // sort
+        .Skip(page * pageSize) // find page
+        .Take(pageSize) // take items
+        .AsEnumerable(); // make enumeratable
+
+    return Task<IEnumerable<T>>.FromResult(result);
         }
 
         /// <summary>
